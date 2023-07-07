de1d66efc76790f9311943be3c9feee6101030dd
Atharv <105142090+atharrvv@users.noreply.github.com>
2023-07-07 17:06:43 +0530

    starte

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Footer(){
+  const year = new Date().getFullYear();
+  <footer>{year}</footer>
+}
+
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<h1>Hello World!</h1>,document.getElementById("root"));
+ReactDOM.render(<App></App>,document.getElementById("root"));
